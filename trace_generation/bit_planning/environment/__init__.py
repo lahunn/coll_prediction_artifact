8c06d9d82867bb43ae59b6d39e5f98dcc7529aa0
兰珲 <landerhuiyun@outlook.com>
2025-10-10 21:16:17 +0800

    移动了一些不需要使用的文件

--- trace_generation/bit_planning/environment/__init__.py
+++ trace_generation/bit_planning/environment/__init__.py
@@ -1,8 +1,8 @@
 from .env_config import *
 from .maze_env import *
 from .kuka_env import *
-from .kuka_2arm_env import *
-from .snake_env import *
+from .unused.kuka_2arm_env import *
+from .unused.snake_env import *
 from .ur5_env import *
 
 strs = ['maze2', 'kuka7', 'snake7', 'kuka13', 'ur5', 'kuka14']
